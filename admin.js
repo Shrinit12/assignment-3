6348433e00ed0bda7f66bc7aafa7b4ab7dad4b3d
Shrinit12 <106505447+Shrinit12@users.noreply.github.com>
2022-11-18 20:16:59 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,20 @@
+const path= require('path');
+
+
+const express= require('express');
+
+const router=express.Router();
+const rootdir=require('../util/path');
+
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(rootdir,'views','add-product.html'));
+     
+});
+
+
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports=router;
